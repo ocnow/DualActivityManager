834e25619edfe6854736a9bc89a2e17d62614410
ocnow <ockulkarni97@gmail.com>
2021-06-29 19:27:24 +0530

    update on 29 eod

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,25 +6,30 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppWidgetProvider{
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
-        for(int i=0; i<appWidgetIds.length; i++){
-            int currentWidgetId = appWidgetIds[i];
-            String url = "http://www.tutorialspoint.com";
+        Log.d("mainAct","hello running update");
+        int number = (new Random().nextInt(100));
+        for (int appWidgetId : appWidgetIds) {
 
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse(url));
 
-            PendingIntent pending = PendingIntent.getActivity(context, 0,intent, 0);
             RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.activity_main);
+            views.setTextViewText(R.id.text1, String.valueOf(number));
+            appWidgetManager.updateAppWidget(appWidgetId, views);
 
-            views.setOnClickPendingIntent(R.id.button, pending);
-            appWidgetManager.updateAppWidget(currentWidgetId,views);
-            Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        //Log.u(Log.INFO,"activity","let try log hisdfs");
+        Log.i("aciti","let try this");
+        super.onEnabled(context);
     }
 }
