b758a55408c8da5437b6a33dabdfcf182ff2adac
ocnow <ockulkarni97@gmail.com>
2021-07-21 23:26:22 +0530

    update on 29 eod

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,12 +7,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppWidgetProvider{
+
+    private static final String MY_BUTTTON_START = "myButtonStart";
+
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
         Log.d("mainAct","hello running update");
         int number = (new Random().nextInt(100));
@@ -21,6 +26,16 @@ public class MainActivity extends AppWidgetProvider{
 
             RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.activity_main);
             views.setTextViewText(R.id.text1, String.valueOf(number));
+
+            Intent intent = new Intent(context, MainActivity.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            views.setOnClickPendingIntent(R.id.imagebutton,
+                    getPendingSelfIntent(context, MY_BUTTTON_START));
+
             appWidgetManager.updateAppWidget(appWidgetId, views);
 
         }
@@ -31,5 +46,21 @@ public class MainActivity extends AppWidgetProvider{
         //Log.u(Log.INFO,"activity","let try log hisdfs");
         Log.i("aciti","let try this");
         super.onEnabled(context);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.w("Widget", "Clicked button1");
+        if (MY_BUTTTON_START.equals(intent.getAction())) {
+            // your onClick action is here
+            Log.w("Widget", "recived button1");
+            Toast.makeText(context, "Click", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
--- app/src/main/java/com/example/myapplication/MainMainActivity.java
+++ app/src/main/java/com/example/myapplication/MainMainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        Log.i("bos","lets goooooooooo");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmain);
     }
