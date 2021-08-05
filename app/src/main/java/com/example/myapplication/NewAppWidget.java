6398a0aa6eb3b90d761e25b5f10dfc2d54463294
ocnow <ockulkarni97@gmail.com>
2021-08-06 00:54:01 +0530

    update on 06/08 EOD

--- app/src/main/java/com/example/myapplication/NewAppWidget.java
+++ app/src/main/java/com/example/myapplication/NewAppWidget.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -18,23 +19,102 @@ import java.util.Date;
 public class NewAppWidget extends AppWidgetProvider {
 
     private static final String mSharedPrefFile =
-            "com.example.android.appwidgetsample";
+            "com.example.myapplication";
 
     private static final String COUNT_KEY = "count";
+
+    private static final String ACTIVITY1_STATE = "ACT1_STATE";
 
     public static String ACTION_SHOWMAIN="ActionShowMain";
 
     @Override
-    public void onReceive(Context ctxt, Intent intent) {
-       Log.d("lolwa",intent.getAction());
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+
     }
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        remoteViews.setOnClickPendingIntent(R.id.imageButton, buildButtonPendingIntent(context,appWidgetId));
+
+        pushWidgetUpdate(context, remoteViews,appWidgetId);
+    }
+
+    public static PendingIntent buildButtonPendingIntent(Context context,int appWidgetId) {
+        Intent intent = new Intent();
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        intent.setAction(ACTION_SHOWMAIN);
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    public static void pushWidgetUpdate(Context context, RemoteViews remoteViews,int appWidgetId) {
+        //ComponentName myWidget = new ComponentName(context, NewAppWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(appWidgetId, remoteViews);
+    }
+    /*@Override
+    public void onReceive(Context context, Intent intent) {
+       Log.d("trynow","well lets try");
+       if(intent.getAction().equals(ACTION_SHOWMAIN)){
+          *//**//* AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+           Log.d("showmain","well lets try");
+           int appWidgetId = intent.getIntExtra("appWidgetId",-1);
+           RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+           views.setInt(R.id.imageButton, "setBackgroundResource", R.drawable.ic_action_name);
+           appWidgetManager.updateAppWidget(appWidgetId, views);*//**//*
+           //int appWidgetId = intent.getIntExtra("appWidgetId",-1);
+          *//**//* RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+           views.setInt(R.id.imageButton, "setImageResource",R.drawable.ic_action_name);
+           views.set*//**//*
+          // appWidgetManager.updateAppWidget(appWidgetId, views);
+           //AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+           int appWidgetId = intent.getIntExtra("sample",-1);
+           Log.d("appwidgetId","appwid id is "+appWidgetId);
+           //updateAppWidget(context,appWidgetManager,appWidgetId);
+       }
+       super.onReceive(context,intent);
+    }
+*//*
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Log.d("appwidgetId","appwid id is "+appWidgetId);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+        Intent active = new Intent(context, NewAppWidget.class);
+        active.putExtra("sample",111);
+        active.setAction(ACTION_SHOWMAIN);
+        PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, 234, active, 0);
+
+       *//* SharedPreferences prefs = context.getSharedPreferences(
+                mSharedPrefFile, 0);
+        if(prefs.getInt(ACTIVITY1_STATE , 0) == 0){
+            views.setImageViewResource(R.id.imageButton, R.drawable.ic_action_name);
+            SharedPreferences.Editor prefEditor = prefs.edit();
+            prefEditor.putInt(ACTIVITY1_STATE, 1);
+            prefEditor.apply();
+        }else{
+            views.setImageViewResource(R.id.imageButton, R.drawable.ic_stop);
+            SharedPreferences.Editor prefEditor = prefs.edit();
+            prefEditor.putInt(ACTIVITY1_STATE, 0);
+            prefEditor.apply();
+        }*//*
 
 
-        SharedPreferences prefs = context.getSharedPreferences(
+
+        //views.setInt(R.id.imageButton, "setImageResource",R.drawable.ic_stop);
+        views.setOnClickPendingIntent(R.id.imageButton, actionPendingIntent);
+
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+       *//* SharedPreferences prefs = context.getSharedPreferences(
                 mSharedPrefFile, 0);
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
@@ -44,9 +124,9 @@ public class NewAppWidget extends AppWidgetProvider {
         PendingIntent actionPendingIntent = PendingIntent.getBroadcast(context, 234, active, 0);
         views.setOnClickPendingIntent(R.id.imageButton, actionPendingIntent);
 
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        appWidgetManager.updateAppWidget(appWidgetId, views);*//*
 
-        /*SharedPreferences prefs = context.getSharedPreferences(
+        *//*SharedPreferences prefs = context.getSharedPreferences(
                 mSharedPrefFile, 0);
         int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
         count++;
@@ -58,10 +138,11 @@ public class NewAppWidget extends AppWidgetProvider {
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
+        *//**//*views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
         views.setTextViewText(R.id.appwidget_update,
                 context.getResources().getString(
-                        R.string.date_count_format, count, dateString));
+                        R.string.date_count_format, count, dateString));*//**//*
+        Log.d("lolwan","hello world");
 
         SharedPreferences.Editor prefEditor = prefs.edit();
         prefEditor.putInt(COUNT_KEY + appWidgetId, count);
@@ -79,7 +160,7 @@ public class NewAppWidget extends AppWidgetProvider {
 
         views.setOnClickPendingIntent(R.id.button_update,pendingUpdate);
         // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);*/
+        appWidgetManager.updateAppWidget(appWidgetId, views);*//*
     }
 
     @Override
@@ -88,6 +169,6 @@ public class NewAppWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-    }
+    }*/
 
 }
